2b1e33fd16dc1edd76d3c9b3e55ea5b4cee866e5
kimjinseop13579 <kimjinseop090@gmail.com>
2026-02-24 16:55:48 +0900

    20260224

--- kim.testList/testList.js
+++ kim.testList/testList.js
@@ -1,5 +1,5 @@
 fetch("questions.json")
-  .then((res) => res.json())
+  .then((res) => res.json()) 
   .then((data) => {
     localStorage.setItem("questions", JSON.stringify(data));
 
@@ -7,27 +7,23 @@ fetch("questions.json")
 
     if (qData) {
       const body = document.getElementById("questionBody");
-
+      
       qData.forEach((qData) => {
         const rows = document.createElement("tr");
-
+        // tr 태그를 생성해서 rows 변수에 저장
         rows.innerHTML = `
         <td>${qData.id + "."}</td>
         <td>${qData.testName}</td>
-        `;
-
+        `;//tr 태그 안에 td 태그를넣고
+        // 각 td태그안에 qData에 id 와 testname을 넣는다
         body.appendChild(rows);
-
-
-
+        //rows(tr) 를 body 변수 안에 추가한다 
       });
 
-    const randomQ = [...data].sort(() => Math.random() - 0.5);
+      const randomQ = [...data].sort(() => Math.random() - 0.5);
 
-    const randomFive = randomQ.slice(0, 5);
+      const randomFive = randomQ.slice(0, 5);
 
-    localStorage.setItem("random", JSON.stringify(randomFive));
-
+      localStorage.setItem("random", JSON.stringify(randomFive));
     }
-    
   });
